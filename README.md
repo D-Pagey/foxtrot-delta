92ae8a2d6b3dac606816dd067551ae73d2b83880
Dan Page <dan.page91@gmail.com>
2022-05-30 00:58:30 +0100

    feat(menu): handle standalone products

--- README.md
+++ README.md
@@ -17,17 +17,15 @@ I use `yarn` but feel free to use `npm` equivalent scripts.
 - [x] Render menu item description
 - [x] Render menu item price
 - [x] Mobile first, responsive layout
-- [ ] Handle `isMasterOptionSet` to handle as standalone product
-- [ ] Handle ignoring `MenuSectionItem` when `isMasterOptionSet` is true
+- [x] Handle `isMasterOptionSet` to handle as standalone product
+- [x] Handle ignoring `MenuSectionItem` when `isMasterOptionSet` is true
 
 ## Developer notes
 
 I would have liked to spend more time on this but my time was limited. Here are some considerations and things I would have liked to do with more time:
 
-- With more time, I would have liked to deal with the `isMasterOptionSet`
 - I would have liked to lazy load images, especially those out of initial view
 - SSR seemed appropriate given the potential SEO benefits
 - I added a snapshot test as there's no functionality to test
-- In a production setting, I would split the menu into manageable components. However, given the small scope, I left it in one page
 - If there were multiple pages, I would have abstracted the `Head` and meta data into a `PageLayout` component
 - Lots of data in the API that I could have done something creative with with more time
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { GetServerSideProps, NextPage } from "next";
 import axios from "axios";
 import Head from "next/head";
+import Image from "next/image";
 
 import { Menu } from "../types";
-import Image from "next/image";
+import { MenuItem } from "../components/MenuItem";
 
 type Props = {
   menuData: Menu;
@@ -32,36 +33,41 @@ const Home: NextPage<Props> = ({ menuData }) => {
 
               {/* List of menu items */}
               {section.MenuItems.map((menuItem) => {
-                const { Price, PublicId, Name, Description, ImageUrl } =
-                  menuItem;
+                const {
+                  Price,
+                  PublicId,
+                  Name,
+                  Description,
+                  ImageUrl,
+                  MenuItemOptionSets,
+                } = menuItem;
 
-                const formattedPrice = new Intl.NumberFormat(undefined, {
-                  style: "currency",
-                  currency: "GBP",
-                }).format(Price);
+                const standaloneOptions = MenuItemOptionSets.find(
+                  (option) => option.IsMasterOptionSet
+                );
+
+                if (standaloneOptions) {
+                  return standaloneOptions.MenuItemOptionSetItems.map(
+                    (option) => (
+                      <MenuItem
+                        key={option.PublicId}
+                        Price={option.Price}
+                        Name={`${Name} - ${option.Name}`}
+                        Description={Description}
+                        ImageUrl={ImageUrl}
+                      />
+                    )
+                  );
+                }
 
                 return (
-                  <div
+                  <MenuItem
                     key={PublicId}
-                    className="grid items-center gap-x-4 border-b py-4 px-2 cursor-pointer hover:shadow rounded"
-                    style={{ gridTemplateColumns: "1fr max-content" }}
-                  >
-                    <p className="col-start-1 row-start-1 font-bold">{Name}</p>
-                    <p className="col-start-1 row-start-2">{Description}</p>
-                    <p className="col-start-1 row-start-3">{formattedPrice}</p>
-                    {ImageUrl && (
-                      <div className="relative flex col-start-2 row-start-1 row-end-4 rounded overflow-hidden">
-                        <Image
-                          src={ImageUrl}
-                          alt={Description}
-                          layout="fixed"
-                          width="100"
-                          height="100"
-                          objectFit="cover"
-                        />
-                      </div>
-                    )}
-                  </div>
+                    Price={Price}
+                    Name={Name}
+                    Description={Description}
+                    ImageUrl={ImageUrl}
+                  />
                 );
               })}
             </li>
